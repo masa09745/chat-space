757a169bb3ab65ffd9e06d819009abc1d1cea653
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2019-10-07 19:09:14 +0900

    インクリメンタルサーチでユーザーの追加削除

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -3,7 +3,8 @@ $(document).on('turbolinks:load', function(){
   var search_list = $("#user-search-result");
 
   function appendbuildHTML(user){
-    var html = `<div class="chat-group-user clearfix">
+    var html = `
+                <div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add js-add-btn" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
                 </div>`;
@@ -14,6 +15,19 @@ $(document).on('turbolinks:load', function(){
     var html = `<div class='chat-group-user clearfix'>${ user }</div>`
     search_list.append(html);
   }
+
+  var member_list = $("#chat-group-users");
+
+  function addUser(userId,userName) {
+    var html = `
+                <div id='chat-group-users'>
+                  <div class='chat-group-user clearfix js-chat-member' id='${userId}'>
+                    <input name='group[user_ids][]' type='hidden' value='${userId}'>
+                      <p class='chat-group-user__name'>${userName}</p>
+                      <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`;
+    member_list.append(html);
+    }
 
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
@@ -39,5 +53,17 @@ $(document).on('turbolinks:load', function(){
     .fail(function(){
       alert('検索に失敗しました');
     })
+  });
+
+  $('#user-search-result').on('click','.user-search-add', function(){
+    $('#chat-group-users').val();
+    var userId = $(this).data('user-id');
+    var userName = $(this).data('user-name');
+    addUser(userId,userName);
+    $(this).parent().remove();
+    });
+
+  $('#chat-group-users').on('click','.user-search-remove',function(){
+    $(this).parent().remove();
   });
 });
