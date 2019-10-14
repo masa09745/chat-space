3c4cb98d812a038ff49d2b0af74a452b8bd95d04
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2019-10-15 00:50:26 +0900

    Update index.js

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -49,7 +49,7 @@ $(document).on('turbolinks:load', function(){
     $.ajax({
       type: 'GET',
       url: '/users',
-      data:  {keyword: input},
+      data:  {keyword: input, users_id: users_id},
       dataType: 'json'
     })
 
