e173e3042b4f0c8ded87466e70b367190499bda4
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2019-10-15 19:33:43 +0900

    Update index.js

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -30,8 +30,8 @@ $(document).on('turbolinks:load', function(){
   function removeUser(user) {
     var html = `
                 <div class="chat-group-user clearfix">
-                  <p class="chat-group-user__name">${ user.name }</p>
-                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                  <p class="chat-group-user__name">${ user.username }</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.userid}" data-user-name="${user.username}">追加</a>
                  </div>`;
     search_list.append(html);
    }
